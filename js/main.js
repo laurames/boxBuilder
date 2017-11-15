70351b15574736e78629dbb2cfd78483cae3caa6
Laura Meskanen-Kundu <laura.meskanen@gmail.com>
2017-11-15 20:48:20 +0200

    added all generated meshes into objects array and added to Readme

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ teacher: Markku Reunanen & Jukka Eerikäinen
 by Laura Meskanen-Kundu
 
 
- _                 _           _ _     _           
-| |__   _____  __ | |__  _   _(_) | __| | ___ _ __ 
+ _                 _           _ _     _
+| |__   _____  __ | |__  _   _(_) | __| | ___ _ __
 | '_ \ / _ \ \/ / | '_ \| | | | | |/ _` |/ _ \ '__|
-| |_) | (_) >  <  | |_) | |_| | | | (_| |  __/ |   
-|_.__/ \___/_/\_\ |_.__/ \__,_|_|_|\__,_|\___|_|   
-                                                   
+| |_) | (_) >  <  | |_) | |_| | | | (_| |  __/ |
+|_.__/ \___/_/\_\ |_.__/ \__,_|_|_|\__,_|\___|_|
+
 */
 
 //global variables
@@ -42,11 +42,11 @@ function init(){
   renderer.setClearColor(0xfdfdfd);
   renderer.setSize(window.innerWidth, window.innerHeight-containerOffSet);
   renderer.setPixelRatio(window.devicePixelRatio);
-  
+
   //camera is always at postion (0, 0, 0):
   camera = new THREE.PerspectiveCamera(35, window.innerWidth/(window.innerHeight-containerOffSet), 0.1, 10000);
   camera.position.z = 1;
-  
+
   //trackball:
   controls = new THREE.TrackballControls( camera );
   controls.rotateSpeed = 1.0;
@@ -56,15 +56,15 @@ function init(){
   controls.noPan = false;
   controls.staticMoving = true;
   controls.dynamicDampingFactor = 0.3;
-  
+
   //create the scene:
   scene = new THREE.Scene();
-  
+
   //the light:
   ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
   pointLight = new THREE.PointLight(0xffffff, 0.5);
   scene.add(ambientLight, pointLight);
-  
+
   //creating a rayCaster that will catch objects so we can move them
   //it is there but cast into the wrong place
   raycaster = new THREE.Raycaster();
@@ -73,13 +73,13 @@ function init(){
   // Initialize audio
   popSound = new Audio('../sounds/pop.wav');
   popSound.volume = 1;
-  
+
   //start the animation loop:
   requestAnimationFrame( animate );
-  
+
   document.addEventListener( 'mousedown', onDocumentMouseDown, false );
   document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-  
+
   window.addEventListener( 'resize', onWindowResize, false );
 }
 
@@ -110,22 +110,22 @@ document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(e) {
   var keyCode = event.which;
   if(selected != undefined || selected != null){
-    if(e.keyCode == 39) {      // moving to right
+    if(e.keyCode == 39) {      // moving to right (arrow >)
       selected.object.position.x += xSpeed;
     }
-    else if(e.keyCode == 37) { // moving left
+    else if(e.keyCode == 37) { // moving left (arrow <)
       selected.object.position.x -= xSpeed;
     }
-    else if(e.keyCode == 38) { // moving up
+    else if(e.keyCode == 38) { // moving up (arrow ^)
       selected.object.position.y += ySpeed;
     }
-    else if(e.keyCode == 40) { // moving down
+    else if(e.keyCode == 40) { // moving down (arrow v)
       selected.object.position.y -= ySpeed;
     }
-    else if(e.keyCode == 90) { // rotate object z towards
+    else if(e.keyCode == 90) { // rotate object z towards (z)
       selected.object.position.z += zSpeed;
     }
-    else if(e.keyCode == 88) { // rotate object z behind
+    else if(e.keyCode == 88) { // rotate object z behind (x)
       selected.object.position.z -= zSpeed;
     }
   }
@@ -150,12 +150,10 @@ function onDocumentMouseDown( event ) {
   var intersects = raycaster.intersectObjects( objects );
   if( event.clientY > containerOffSet ){
     if ( intersects.length > 0 ) {
-      console.log("in raycaster");
       popSound.play();
       selected = intersects[ 0 ];
       intersects[ 0 ].object.material.wireframe = true;
     }else{
-      console.log("in else");
       removeWireframe();
     }
   }
@@ -174,20 +172,20 @@ function addCube(){
   //the cube
   var geometry = new THREE.BoxGeometry(1, 1, 1);
   var material = new THREE.MeshLambertMaterial(); //one can place any color
-  
+
   objects.push(new THREE.Mesh(geometry,material));
   objects[objects.length-1].material.color.setHex(cubeColor);
   objects[objects.length-1].position.set(0, 0, -10);
   objects[objects.length-1].name = counter;
   console.log(objects);
   counter++;
-  
+
   if(objects.length > 0){
     document.getElementById("removeEverything").disabled = false;
     document.getElementById("dancingCubes").disabled = false;
   }
-  
-  console.log("selected is: " + selected);
+
+  //console.log("selected is: " + selected);
   camera.position.set (0, 0, 1);
   scene.add(objects[objects.length-1]);
 }
@@ -195,7 +193,6 @@ function addCube(){
 //removes cubes from scene
 function removeCube(selected) {
   if(selected != undefined || selected != null){
-    console.log("in remove");
     scene.remove(scene.getObjectByName(selected.object.name));
     objects.splice(objects[selected],1)
     console.log(objects);
@@ -212,7 +209,7 @@ function setCubeColor(picker){
 }
 
 function removeEverything(){
-  while(scene.children.length > 0){ 
+  while(scene.children.length > 0){
     scene.remove(scene.children[0]);
     objects = [];
   }
@@ -235,7 +232,7 @@ function getRandomColor() {
 function lotsOfCubes(){
   var geometry = new THREE.BoxGeometry(1, 1, 1);
   var material = new THREE.MeshLambertMaterial();
-  
+
   for ( var i = 0; i < 50; i ++ ) {
     var mesh = new THREE.Mesh( geometry, material );
     mesh.material.color.setHex('0x'+getRandomColor());
@@ -245,21 +242,30 @@ function lotsOfCubes(){
     mesh.updateMatrix();
     mesh.matrixAutoUpdate = true;
     camera.position.set (0, 0, -15);
+    if(i === 49){
+      for( var x = 2; x < scene.children.length-2; x++) {
+        objects.push(scene.children[x]);
+      }
+
+      //console.log(mesh);
+      console.log(scene.children.slice(2, scene.children.length) );
+      console.log(objects);
+    }
     scene.add( mesh );
   }
   if(scene.children.length > 0){
     document.getElementById("removeEverything").disabled = false;
     document.getElementById("dancingCubes").disabled = false;
   }
-  
-  console.log(scene);
+
+  //console.log(scene);
 }
 
 function dancingCubes(){
   song = new stasilo.BeatDetector({
          sens: 				      5.0,
-         visualizerFFTSize: 256, 
-         analyserFFTSize:   256, 
+         visualizerFFTSize: 256,
+         analyserFFTSize:   256,
          passFreq:          600,
          url:               "sounds/Black_Ant_realest_year_9.mp3" } );
   dance = true;
--- readme.md
+++ readme.md
@@ -3,11 +3,25 @@
 ## Run
 just oppen index.html and play around
 
+## How to use
+arrow keys are used for x and y coordinate movement of created boxes.
+< = left (x)
+> = right (x)
+^ = up (y)
+v = down (y)
+z = inside (-z)
+x = out (+z)
+
 ## Code
 Main code is in js/main.js and the comments should explain what is happening
 
 ## Notes
 There might still be small bugs with raycaster in the 3D space but i’ve attached everything as .zip and also a small video to quickly show how it works.
 
+Use Brackets inbuilt "Live Preview" to view the project if you don't want to 1.Setup a local server 2.Place Chrome security extensions or something else to overcome the error "Cross origin requests are only supported for protocol schemes: http, data, chrome, chrome-extension, https."
+
 ## Demo
 Can be found on vimeo: https://vimeo.com/241652027
+
+## Improvements
+It would be great to manipulate all the individual auto generated boxes with changing their color etc. Also changing the dimensions of the individual boxes. Having the raycaster behave expectedly. Maybe add own music. Save the file to share with others... So many ideas so little time :)
